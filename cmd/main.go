d7eae1e2e4685c20e78aee700e20c46d8052a77a
Kaung Myat Han <kaungmyathan2222@gmail.com>
2024-02-25 11:36:48 +0630

    main file setup with gateway events

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"os"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbiface"
+	"github.com/kaungmyathan22/golang-dynamodb-serverless-user-crud/pkg/handlers"
+)
+
+var (
+	dynaClient dynamodbiface.DynamoDBAPI
+)
 
 func main() {
-	fmt.Println("Hola amigos")
+	region := os.Getenv("AWS_REGION")
+	awsSession, err := session.NewSession(&aws.Config{Region: aws.String(region)})
+	if err != nil {
+		panic(err.Error())
+	}
+	dynamoClient = dynamodb.New(awsSession)
+	lambda.Start(handler)
+}
+
+const tableName = "lambdaInGoUser"
+
+func handler(req events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
+	switch req.HTTPMethod {
+	case "GET":
+		return handlers.GetUser(req, tableName, dynaClient)
+	case "POST":
+		return handlers.CreateUser(req, tableName, dynaClient)
+	case "PUT":
+		return handlers.UpdateUser(req, tableName, dynaClient)
+	case "DELETE":
+		return handlers.DeleteUser(req, tableName, dynaClient)
+	default:
+		return handlers.UnhandledMethod()
+	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module github.com/kaungmyathan22/golang-dynamodb-serverless-user-crud
 
 go 1.21.5
+
+require github.com/aws/aws-sdk-go v1.50.25
+
+require (
+	github.com/aws/aws-lambda-go v1.46.0
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
+)
